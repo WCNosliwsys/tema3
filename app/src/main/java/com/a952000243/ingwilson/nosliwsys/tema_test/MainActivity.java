e8ff57ec341a84c178f0d109509084c426b269dd
Ing Wilson <nosliwsys@gmail.com>
2019-04-07 05:18:20 -0500

    Sabado noche

--- app/src/main/java/com/a952000243/ingwilson/nosliwsys/tema_test/MainActivity.java
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/tema_test/MainActivity.java
@@ -1,13 +1,50 @@
 package com.a952000243.ingwilson.nosliwsys.tema_test;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends FragmentActivity
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    GoogleMap mapa;
+    LatLng ubicacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        ubicacion = new LatLng(-18.024578408795737, -70.25051112093377); //Nos ubicamos en la UNJBG
+        mapa.addMarker(new MarkerOptions().position(ubicacion).title("Marcador UNJBG"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+        mapa.setOnMapClickListener(this);
+    }
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+    }
+    public void addMarker(View view) {
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
+    }
+    @Override public void onMapClick(LatLng puntoPulsado) {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+
 }
